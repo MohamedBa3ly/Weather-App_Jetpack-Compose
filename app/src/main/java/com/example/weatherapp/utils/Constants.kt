bddc7150244594e208ada39839fba05cba1b7144
Mohamed Ahmed <103647107+MohamedBa3ly@users.noreply.github.com>
2023-03-22 11:15:49 +0200

    Update Constants.kt

--- app/src/main/java/com/example/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/example/weatherapp/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.weatherapp.utils
 
 object Constants {
     const val BASE_URL = "http://api.weatherapi.com/"
-    const val API_KEY = "2e234959b88141fabf0184817233001"
+    const val API_KEY = "Your_Api_Key"
 }
